4c1af7588e44a93ae4da47f52f94075e1b68d6d8
YinSQ <18620895340@qq.com>
2018-09-15 11:56:35 +0800

    update for cache
    update for cache

--- WeChat/Util/Cache/SQCache.swift
+++ WeChat/Util/Cache/SQCache.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+public enum CacheError: Error {
+    case cacheClassTypeError
+    case noCache
+}
+
+class SQCache: NSObject {
+    
+    public static let shared = SQCache()
+    
+    private var dataCache = NSCache<NSString, AnyObject>()
+    
+    private override init() {
+        
+    }
+    
+}
+
+extension SQCache {
+    
+    public func setCache(key: NSString, value: AnyObject) {
+        dataCache.setObject(value, forKey: key)
+    }
+    
+    public func getCache<T: AnyObject>(key: NSString, type: T.Type) throws -> T {
+        if let value = dataCache.object(forKey: key) {
+            if value is T {
+                return value as! T
+            } else {
+                throw CacheError.cacheClassTypeError
+            }
+        }
+        throw CacheError.noCache
+    }
+    
+    public func getCacheList<T: AnyObject>(key: NSString, type: [T].Type) throws -> [T] {
+        // TODO: data 转 JSON 再转 Model
+        if let value = dataCache.object(forKey: key) {
+            if value is Array<T> {
+                return value as! [T]
+            } else {
+                throw CacheError.cacheClassTypeError
+            }
+        }
+        throw CacheError.noCache
+    }
+    
+}
